b0ce9ea533cf964649c5502b558831f65bff52ef
Elliot <elliot.ayrey@gmail.com>
2025-10-01 03:36:54 +0100

    chromecast: keep-alive
    Can now decode messages, mannually and using pb_decode, and respond to
    PING with PONG.

--- main.c
+++ main.c
@@ -23,9 +23,25 @@
 
 #include <poll.h>
 
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+
 static char g__chromecast_ip[INET6_ADDRSTRLEN];
 static bool g__chromecast_found;
 static uint8_t send_buf[4094];
+
+static const char *connection_ns = "urn:x-cast:com.google.cast.tp.connection";
+static const char *heartbeat_ns = "urn:x-cast:com.google.cast.tp.heartbeat";
+static const char *receiver_ns = "urn:x-cast:com.google.cast.receiver";
+static const char *connect_msg = "{\"type\": \"CONNECT\"}";
+// static const char *ping_msg = "{\"type\": \"PING\"}";
+static const char *pong_msg = "{\"type\": \"PONG\"}";
+static const char *get_status_msg = "{\"type\": \"GET_STATUS\", \"requestId\": 17}";
+// static const char *get_app_availability_msg = "{\"type\": \"GET_APP_AVAILABILITY\", \"appId\": \"CC1AD845\"}";
+
 
 #define openssl_dump_err() \
     do { \
@@ -43,6 +59,23 @@ encode_string (pb_ostream_t *stream, const pb_field_iter_t *field, void * const 
 
     if (pb_encode_tag_for_field (stream, field))
         return pb_encode_string (stream, (unsigned char *) str, strlen (str));
+
+    return false;
+}
+
+/*
+ * https://jpa.kapsi.fi/nanopb/docs/concepts.html#decoding-callbacks
+ */
+static bool
+decode_string (pb_istream_t *stream, const pb_field_iter_t *field, void **arg)
+{
+    if (field && PB_LTYPE (field->type) == PB_LTYPE_STRING)
+    {
+        *arg = malloc (stream->bytes_left + 1);
+        memset (*arg, 0, stream->bytes_left + 1);
+
+        return pb_read (stream, *arg, stream->bytes_left);
+    }
 
     return false;
 }
@@ -154,26 +187,6 @@ http_read (int sk)
     }
 }
 
-static void
-parse_recv_msg (char *buf, size_t len)
-{
-    extensions_api_cast_channel_CastMessage rmsg = extensions_api_cast_channel_CastMessage_init_zero;
-    pb_istream_t stream = pb_istream_from_buffer ((uint8_t *) buf, len);
-    int ret = pb_decode (&stream, extensions_api_cast_channel_CastMessage_fields, &rmsg);
-
-    if (ret)
-    {
-        printf ("version        : %d\n", rmsg.protocol_version);
-        printf ("source-id      : %s\n", (char *) rmsg.source_id.arg);
-        printf ("destination-id : %s\n", (char *) rmsg.destination_id.arg);
-
-    }
-    else
-    {
-        printf ("Failed to decode message: %s\n", PB_GET_ERROR (&stream));
-    }
-}
-
 static char
 ascii_ (uint8_t val)
 {
@@ -182,11 +195,260 @@ ascii_ (uint8_t val)
     return ' ';
 }
 
+static bool
+send_msg (SSL *ssl, uint8_t *send_buf, size_t send_len, const char *namespace, const char *payload)
+{
+    memset (send_buf, 0, send_len);
+
+    extensions_api_cast_channel_CastMessage msg = extensions_api_cast_channel_CastMessage_init_zero;
+    pb_ostream_t stream = pb_ostream_from_buffer (send_buf + 4, send_len - 4);
+    bool ok ;
+
+    msg.protocol_version = extensions_api_cast_channel_CastMessage_ProtocolVersion_CASTV2_1_0;
+    msg.source_id.arg = (void *) "sender-0";
+    msg.source_id.funcs.encode = encode_string;
+    msg.destination_id.arg = (void *) "receiver-0";
+    msg.destination_id.funcs.encode = encode_string;
+    msg.namespace.arg = (void *) namespace;
+    msg.namespace.funcs.encode = encode_string;
+    msg.payload_type = extensions_api_cast_channel_CastMessage_PayloadType_STRING;
+    msg.payload_utf8.arg = (void *) payload;
+    msg.payload_utf8.funcs.encode = encode_string;
+
+    ok = pb_encode (&stream, extensions_api_cast_channel_CastMessage_fields, &msg);
+
+    printf ("encode ok=%d\n", ok);
+
+    if (ok)
+    {
+        printf ("bytes written: %u\n", stream.bytes_written);
+
+        send_buf[0] = (stream.bytes_written >> 24) & 0xFF;
+        send_buf[1] = (stream.bytes_written >> 16) & 0xFF;
+        send_buf[2] = (stream.bytes_written >>  8) & 0xFF;
+        send_buf[3] = (stream.bytes_written >>  0) & 0xFF;
+
+        size_t wr;
+        ok = SSL_write_ex (ssl, send_buf, stream.bytes_written + 4, &wr);
+        printf ("SSL write pb-len=%u (0x%x) wr=%zu ok=%d\n", stream.bytes_written, stream.bytes_written, wr, ok);
+
+        printf ("----------------\n");
+        fwrite (send_buf, 1, wr, stdout);
+        printf ("\n");
+        printf ("----------------\n");
+
+        char line[512];
+        int l = 0;
+        char *p = line;
+
+        for (int i = 0; i < wr; i++)
+        {
+            if ((i % 8) == 0 && i != 0)
+            {
+                printf ("%02d: %s\n", l++, line);
+                p = line;
+            }
+
+            p += sprintf (p, "0x%02x(%c) ", send_buf[i], ascii_ (send_buf[i]));
+        }
+
+        if (p != line)
+        {
+            printf ("%02d: %s\n", l, line);
+        }
+        printf ("----------------\n");
+    }
+
+    return ok;
+}
+
+enum wire_type
+{
+    VARINT = 0,
+    I64,
+    LEN,
+    SGROUP,
+    EGROUP,
+    I32
+};
+
+static char *
+wire_str (u8 wire)
+{
+    char *str;
+
+    switch (wire)
+    {
+        case VARINT: { str = "VARINT"; } break;
+        case I64:    { str = "I64";    } break;
+        case LEN:    { str = "LEN";    } break;
+        case SGROUP: { str = "SGROUP"; } break;
+        case EGROUP: { str = "EGROUP"; } break;
+        case I32:    { str = "I32";    } break;
+        default:     { str = "??";     } break;
+    }
+
+    return str;
+}
+
+static char *
+field_str (u8 fieldno)
+{
+    char *str;
+
+    // TODO: replace raw ints with extensions_api_cast_channel_CastMessage_*_tag (maybe..)
+    switch (fieldno)
+    {
+        case 1:  { str = "protocol-version"; } break;
+        case 2:  { str = "source-id";        } break; 
+        case 3:  { str = "destination-id";   } break;
+        case 4:  { str = "namespace";        } break;
+        case 5:  { str = "payload-type";     } break;
+        case 6:  { str = "payload-utf8";     } break;
+        case 7:  { str = "payload-binary";   } break;
+        default: { str = "??";               } break;
+     }
+
+     return str;
+}
+
+struct tag
+{
+    u8 fieldno;
+    u8 wire;
+};
+
+static int
+decode_varint (u8 *buf, u64 *varint)
+{
+    int bytes = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        u8 b = buf[i];
+
+        *varint |= (u64) (b & 0x7F) << (i * 7);
+        bytes++;
+
+        if ((b & 0x80) == 0)
+            break;
+    }
+
+    return bytes;
+}
+
+static int
+decode_tag (u8 *buf, size_t len, struct tag *tag)
+{
+    u64 varint = 0;
+    int bytes = 0;
+
+    bytes += decode_varint (buf, &varint);
+
+    tag->fieldno = varint >> 3;
+    tag->wire = varint & 0x07;
+
+    return bytes;
+}
+
+static void
+parse_recv_msg (u8 *buf, size_t len, SSL *ssl)
+{
+    extensions_api_cast_channel_CastMessage rmsg = extensions_api_cast_channel_CastMessage_init_zero;
+
+    rmsg.source_id.funcs.decode = decode_string;
+    rmsg.destination_id.funcs.decode = decode_string;
+    rmsg.namespace.funcs.decode = decode_string;
+    rmsg.payload_utf8.funcs.decode = decode_string;
+
+    uint32_t rlen = (buf[0] << 24) + (buf[1] << 16) + (buf[2] << 8) + (buf[3] << 0);
+    buf += 4;
+    len -= 4;
+
+    /**
+     * https://protobuf.dev/programming-guides/encoding/
+     *
+     * Field numbers (defined by cast_channel.proto):
+     * 1 = protocol-version
+     * 2 = source_id
+     * 3 = destination_id
+     * 4 = namespace
+     * 5 = payload_type
+     * 6 = payload_utf8
+     * 7 = payload_binary
+     *
+     * Wire-type:
+     * 0 = VARINT
+     * 1 = I64
+     * 2 = LEN
+     * 3 = SGROUP
+     * 4 = EGROUP
+     * 5 = I32
+     *
+     * Tag:
+     *   (field_number << 3) | wire_type
+     * 
+     * 0000 1000
+     */
+
+    int bytes = 0;
+    while (bytes < len)
+    {
+        struct tag tag = {};
+        u64 varint = 0;
+
+        bytes += decode_tag (buf + bytes, len - bytes, &tag);
+        if (tag.wire == VARINT)
+        {
+            bytes += decode_varint (buf + bytes, &varint);
+            printf ("%d %s : %" PRIu64 "\n", tag.fieldno, field_str (tag.fieldno), varint);
+        }
+        else if (tag.wire == LEN)
+        {
+            bytes += decode_varint (buf + bytes, &varint);
+            printf ("%d %s : %s(%" PRIu64 ") '%.*s'\n", tag.fieldno, field_str (tag.fieldno), wire_str (tag.wire), varint, (int) varint, (char *) buf + bytes);
+            bytes += (int) varint;
+        }
+    }
+
+    printf ("consumed %d bytes (out of %d)\n", bytes, len);
+
+    pb_istream_t stream = pb_istream_from_buffer ((uint8_t *) buf, rlen);
+    int ret = pb_decode (&stream, extensions_api_cast_channel_CastMessage_fields, &rmsg);
+
+    printf ("decode ok=%d\n", ret);
+    if (ret)
+    {
+        printf ("version        : %d\n", rmsg.protocol_version);
+        printf ("source-id      : %s\n", (char *) rmsg.source_id.arg);
+        printf ("destination-id : %s\n", (char *) rmsg.destination_id.arg);
+        printf ("namespace      : %s\n", (char *) rmsg.namespace.arg);
+        printf ("payload-utf8   : %s\n", (char *) rmsg.payload_utf8.arg);
+
+        if (strstr (rmsg.payload_utf8.arg, "PING"))
+        {
+            printf ("PING: should send PONG\n");
+            send_msg (ssl, send_buf, sizeof (send_buf), heartbeat_ns, pong_msg);
+            send_msg (ssl, send_buf, sizeof (send_buf), receiver_ns, get_status_msg);
+        }
+    }
+    else
+    {
+        printf ("Failed to decode message: %s\n", PB_GET_ERROR (&stream));
+    }
+
+    free (rmsg.source_id.arg);
+    free (rmsg.destination_id.arg);
+    free (rmsg.namespace.arg);
+    free (rmsg.payload_utf8.arg);
+}
+
+
 static void
 ssl_read (SSL *ssl)
 {
     size_t rd;
-    char recv_buf[512] = {};
+    u8 recv_buf[1024] = {};
 
     if (!ssl)
     {
@@ -196,8 +458,10 @@ ssl_read (SSL *ssl)
 
     while (SSL_read_ex (ssl, recv_buf, sizeof (recv_buf), &rd))
     {
+        printf ("rd=%d\n", rd);
         printf ("----------------\n");
         fwrite (recv_buf, 1, rd, stdout);
+        printf ("\n");
         printf ("----------------\n");
 #if 1
         char line[512];
@@ -208,22 +472,19 @@ ssl_read (SSL *ssl)
         {
             if ((i % 8) == 0 && i != 0)
             {
-                printf ("%d: %s\n", l++, line);
+                printf ("%02d: %s\n", l++, line);
                 p = line;
             }
-            else
-            {
-                p += sprintf (p, "%02x(%c) ", recv_buf[i], ascii_ (recv_buf[i]));
-            }
+            p += sprintf (p, "0x%02x(%c) ", recv_buf[i], ascii_ (recv_buf[i]));
         }
 
         if (p != line)
         {
-            printf ("%d: %s\n", l, line);
+            printf ("%02d: %s\n", l, line);
         }
 #endif
 
-        parse_recv_msg (recv_buf, rd);
+        parse_recv_msg (recv_buf, rd, ssl);
     }
 
     if (SSL_get_error (ssl, 0) != SSL_ERROR_ZERO_RETURN) {
@@ -271,113 +532,48 @@ _query_cb (int sk, const struct sockaddr *from, size_t from_len, mdns_entry_type
 static bool
 mdns_setup (void)
 {
-	int sockets[32];
-	bool ok = false;
+    int sk = mdns_socket_open_ipv4 (NULL);
 
-	sockets[0] = mdns_socket_open_ipv4 (NULL);
-	printf ("mdns: open socket: %d\n", sockets[0]);
-	int n_sks = 1;
-	printf ("mdns: opened %d sockets\n", n_sks);
-	if (n_sks <= 0)
-		return ok;
+    printf ("mdns: open socket: %d\n", sk);
+    printf ("mdns: send discovery (sk=%d)\n", sk);
 
-	for (int i = 0; i < n_sks; i++)
-	{
-		int sk = sockets[i];
-
-		printf ("mdns: send discovery (sk=%d)\n", sk);
-		if (mdns_discovery_send (sk) != 0)
-		{
-			printf ("Failed to send DNS-DS discovery: %s\n", strerror (errno));
-		}
-	}
-
-	unsigned char buf[2048];
-	size_t records = 0;
-	int ret = 0;
-
-	do {
-		int nfds = 0; // IMPORTANT: needs to be +1 higher than the highest socket value
-		fd_set readfds;
-		struct timeval timeout = { .tv_sec = 5 };
-
-		FD_ZERO (&readfds);
-
-		for (int i = 0; i < n_sks; i++)
-		{
-			int sk = sockets[i];
-
-			if (sk >= nfds)
-			{
-				nfds = sk + 1;
-			}
-			FD_SET (sk, &readfds);
-		}
-
-		printf ("mdns: wait for response (nfds=%d)\n", nfds);
-		ret = select (nfds, &readfds, 0, 0, &timeout);
-		printf ("mdns: ret=%d\n", ret);
-		if (ret > 0)
-		{
-            timeout.tv_sec = 1;
-			for (int i = 0; i < n_sks; i++)
-			{
-				int sk = sockets[i];
-				if (FD_ISSET (sk, &readfds))
-				{
-					records += mdns_discovery_recv (sk, buf, sizeof (buf), _query_cb, NULL);
-					printf ("records: %zu\n", records);
-				}
-			}
-		}
-	} while (ret > 0);
-
-	for (int i = 0; i < n_sks; i++)
-	{
-		int sk = sockets[i];
-		mdns_socket_close (sk);
-	}
-
-	return records > 0;
-}
-
-static bool
-send_msg (SSL *ssl, uint8_t *send_buf, size_t send_len, const char *namespace, const char *payload)
-{
-    extensions_api_cast_channel_CastMessage msg = extensions_api_cast_channel_CastMessage_init_zero;
-    pb_ostream_t stream = pb_ostream_from_buffer (send_buf + 4, send_len - 4);
-    bool ok ;
-
-    msg.protocol_version = extensions_api_cast_channel_CastMessage_ProtocolVersion_CASTV2_1_0;
-    msg.source_id.arg = (void *) "sender-0";
-    msg.source_id.funcs.encode = encode_string;
-    msg.destination_id.arg = (void *) "receiver-0";
-    msg.destination_id.funcs.encode = encode_string;
-    msg.namespace.arg = (void *) namespace;
-    msg.namespace.funcs.encode = encode_string;
-    msg.payload_type = extensions_api_cast_channel_CastMessage_PayloadType_STRING;
-    msg.payload_utf8.arg = (void *) payload;
-    msg.payload_utf8.funcs.encode = encode_string;
-
-    ok = pb_encode (&stream, extensions_api_cast_channel_CastMessage_fields, &msg);
-
-    printf ("encode ok=%d\n", ok);
-
-    if (ok)
+    if (mdns_discovery_send (sk) != 0)
     {
-        printf ("bytes written: %u\n", stream.bytes_written);
-
-        send_buf[0] = (stream.bytes_written >> 24) & 0xFF;
-        send_buf[1] = (stream.bytes_written >> 16) & 0xFF;
-        send_buf[2] = (stream.bytes_written >>  8) & 0xFF;
-        send_buf[3] = (stream.bytes_written >>  0) & 0xFF;
-
-        size_t wr;
-        ok = SSL_write_ex (ssl, send_buf, stream.bytes_written, &wr);
-        printf ("SSL write written=%u wr=%zu ok=%d\n", stream.bytes_written, wr, ok);
+        printf ("Failed to send DNS-DS discovery: %s\n", strerror (errno));
     }
 
-    return ok;
+    unsigned char buf[2048];
+    size_t records = 0;
+    int ret = 0;
+
+    struct pollfd pfds[1];
+    int timeout = 5000;
+
+    pfds[0].fd = sk;
+    pfds[0].events = POLLIN;
+
+    do {
+        printf ("mdns: wait for response (timeout=%d)\n", timeout);
+        ret = poll (pfds, 1, timeout);
+        printf ("mdns: ret=%d\n", ret);
+        if (ret > 0)
+        {
+            timeout = 100;
+            for (int i = 0; i < 1; i++)
+            {
+                if (pfds[i].revents & POLLIN)
+                {
+                    int sk = pfds[i].fd;
+                    records += mdns_discovery_recv (sk, buf, sizeof (buf), _query_cb, NULL);
+                    printf ("records: %zu\n", records);
+                }
+            }
+        }
+    } while (ret > 0);
+
+    mdns_socket_close (sk);
+
+    return records > 0;
 }
 
 /*
@@ -391,6 +587,8 @@ chromecast_socket_setup (int *out_sk)
     BIO *bio = NULL;
     int sk;
     bool ok;
+
+    printf ("SSL: socket setup\n");
     
     SSL_library_init ();
     SSL_load_error_strings ();
@@ -434,88 +632,11 @@ chromecast_socket_setup (int *out_sk)
 
                 *out_sk = sk;
 
-                const char *transport_ns = "urn:x-cast:com.google.cast.tp.connection";
-                // const char *receiver_ns = "urn:x-cast:com.google.cast.receiver";
-
-                const char *connect_msg = "{\"type\": \"CONNECT\"}";
-                // const char *get_status_msg = "{\"type\": \"GET_STATUS\"}";
-                if (1)
-                {
-                ok = send_msg (ssl, send_buf, sizeof (send_buf), transport_ns, connect_msg);
+                ok = send_msg (ssl, send_buf, sizeof (send_buf), connection_ns, connect_msg);
                 if (!ok)
                 {
                     printf ("Failed to send message to chromecast\n");
                 }
-                }
-#if 0
-                extensions_api_cast_channel_CastMessage msg = extensions_api_cast_channel_CastMessage_init_zero;
-                pb_ostream_t stream = pb_ostream_from_buffer (send_buf + 4, sizeof (send_buf) - 4);
-                msg.protocol_version = extensions_api_cast_channel_CastMessage_ProtocolVersion_CASTV2_1_0;
-                msg.source_id.arg = (void *) "sender-0";
-                msg.source_id.funcs.encode = encode_string;
-                msg.destination_id.arg = (void *) "receiver-0";
-                msg.destination_id.funcs.encode = encode_string;
-                msg.namespace.arg = (void *) transport_ns;
-                msg.namespace.funcs.encode = encode_string;
-                msg.payload_type = extensions_api_cast_channel_CastMessage_PayloadType_STRING;
-                msg.payload_utf8.arg = (void *) connect_msg;
-                msg.payload_utf8.funcs.encode = encode_string;
-
-                ok = pb_encode (&stream, extensions_api_cast_channel_CastMessage_fields, &msg);
-                printf ("encode=%d\n", ok);
-
-                if (ok)
-                {
-                    printf ("bytes written: %u\n", stream.bytes_written);
-
-                    send_buf[0] = (stream.bytes_written >> 24) & 0xFF;
-                    send_buf[1] = (stream.bytes_written >> 16) & 0xFF;
-                    send_buf[2] = (stream.bytes_written >>  8) & 0xFF;
-                    send_buf[3] = (stream.bytes_written >>  0) & 0xFF;
-
-                    size_t wr;
-                    ok = SSL_write_ex (ssl, send_buf, stream.bytes_written, &wr);
-                    printf ("SSL write: %d\n", ok);
-                }
-#endif
-#if 0
-                if (ok)
-                {
-                    size_t rd;
-                    char recv_buf[512];
-
-                    while (SSL_read_ex (ssl, recv_buf, sizeof (recv_buf), &rd))
-                    {
-                        char line[256];
-                        int l = 0;
-                        char *p = line;
-
-                        printf ("----------------\n");
-                        fwrite (recv_buf, 1, rd, stdout);
-                        printf ("\n");
-                        printf ("----------------\n");
-                        for (int i = 0; i < rd; i++)
-                        {
-                            if ((i % 8) == 0 && i != 0)
-                            {
-                                printf ("%d: %s\n", l++, line);
-                                p = line;
-                            }
-                            else
-                            {
-                                p += sprintf (p, "%02x ", recv_buf[i]);
-                            }
-                        }
-
-                        if (p != line)
-                        {
-                            printf ("%d: %s\n", l, line);
-                        }
-                        printf ("----------------\n");
-                    }
-                    printf ("\n");
-                }
-#endif
             }
             else
             {
@@ -581,6 +702,7 @@ main (int c, char **v)
 
 	while (1)
 	{
+        printf ("\npolling\n");
         int ret = poll (pfds, nfds, -1);
         if (ret == -1)
         {
@@ -592,6 +714,9 @@ main (int c, char **v)
         {
             if (pfds[i].revents & POLLIN)
             {
+                printf ("-----------------------\n");
+                printf ("Received Packet (fd=%d)\n", pfds[i].fd);
+                printf ("-----------------------\n");
                 if (pfds[i].fd == web_sk)
                 {
                     http_read (pfds[i].fd);
